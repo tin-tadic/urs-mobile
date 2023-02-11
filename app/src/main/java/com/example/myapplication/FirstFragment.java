09190b9f245e4dd1824df567cb1981bd26e926fc
Tin Tadić <tin.tadic46@gmail.com>
2023-02-11 17:32:20 +0100

    init

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.myapplication.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = String.valueOf(binding.inputUsername.getText());
+                String password = String.valueOf(binding.inputPassword.getText());
+
+                boolean success = true;
+                if (success) {
+                    Toast.makeText(getContext(), "Successfully logged in!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), "Invalid credentials!", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+                String res = username + " " + password;
+                System.out.println(res);
+
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
