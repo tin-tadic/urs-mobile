a8443393eb30db3beb45c2801a68cd646ea2dda4
Tin Tadić <tin.tadic46@gmail.com>
2023-02-11 20:30:41 +0100

    working submit presence

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +9,22 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.myapplication.databinding.FragmentFirstBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 public class FirstFragment extends Fragment {
 
@@ -35,21 +49,61 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 String username = String.valueOf(binding.inputUsername.getText());
                 String password = String.valueOf(binding.inputPassword.getText());
+                String code = String.valueOf(binding.inputCode.getText());
 
-                boolean success = true;
-                if (success) {
-                    Toast.makeText(getContext(), "Successfully logged in!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getContext(), "Invalid credentials!", Toast.LENGTH_SHORT).show();
+                try {
+                    RequestQueue requestQueue = Volley.newRequestQueue(getContext());
 
+                    JSONObject jsonBody = new JSONObject();
+                    jsonBody.put("username", username);
+                    jsonBody.put("password", password);
+                    jsonBody.put("code", code);
+
+                    final String requestBody = jsonBody.toString();
+
+                    StringRequest stringRequest = new StringRequest(
+                            Request.Method.POST,
+                            "http://10.0.2.2:8085/submitPresence",
+                            response -> {
+                                Log.i("VOLLEY", response);
+                                Toast.makeText(getContext(), response, Toast.LENGTH_SHORT).show();
+                            },
+                            error -> {
+                                Log.e("VOLLEY", error.toString());
+                                Toast.makeText(getContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                    ) {
+                        @Override
+                        public String getBodyContentType() {
+                            return "application/json; charset=utf-8";
+                        }
+
+                        @Override
+                        public byte[] getBody() {
+                            try {
+                                return requestBody == null ? null : requestBody.getBytes("utf-8");
+                            } catch (UnsupportedEncodingException uee) {
+                                VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                                return null;
+                            }
+                        }
+
+                        @Override
+                        protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                            String responseString = "";
+
+                            if (response != null) {
+                                responseString =  new String(response.data , StandardCharsets.UTF_8);
+                            }
+
+                            return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                        }
+                    };
+
+                    requestQueue.add(stringRequest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-
-
-                String res = username + " " + password;
-                System.out.println(res);
-
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
